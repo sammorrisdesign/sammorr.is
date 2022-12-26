3bb54c24b5b9ce3d046d9df96c109f88557d4a6e
sammorrisdesign <mail@sammorrisdesign.co.uk>
2022-12-26 11:49:54 -0500

    fix bs scripts

--- scripts/local.js
+++ scripts/local.js
@@ -4,35 +4,37 @@ var cmd = require('node-cmd');
 
 // create server
 var bs = require('browser-sync').create();
-    bs.init({
-        server: './.build',
-        port: 8500
-    });
+bs.init({
+  server: './.build',
+  port: 8500
+});
 
-    bs.watch('./.build/*.css', function(event, file) {
-        if (event === 'change') {
-            bs.reload('*.css');
-        }
-    });
+bs.watch('./.build/*.css', function (event, file) {
+  if (event === 'change') {
+    bs.reload('*.css');
+  }
+});
 
 // watch src files
-watch(['src', 'data.json'], { recursive: true }, function(event, file) {
-    var fileExt = file.substring(file.lastIndexOf('.') + 1);
-    var isAssets = file.includes('/assets/');
+watch(['src', 'data.json'], { recursive: true }, function (event, file) {
+  var fileExt = file.substring(file.lastIndexOf('.') + 1);
+  var isAssets = file.includes('/assets/');
 
-    if (isAssets) {
-        console.log('updating static assets');
-        cmd.run('npm run compile -- local static', function(data) { console.log(data); });
-    } else if (fileExt === 'html' || fileExt === 'svg' || fileExt === 'json') {
-        console.log('updating html');
-        cmd.run('npm run compile -- local html', function(data) { console.log(data); });
-    } else if (fileExt === 'scss') {
-        console.log('updating css');
-        cmd.run('npm run compile -- local css', function(data) { console.log(data); });
-    } else if (fileExt === 'js') {
-        console.log('updating js');
-        cmd.run('npm run compile -- local js', function(data) { console.log(data); });
-    } else {
-        console.log('non-watchable file extension changed :' + fileExt);
-    }
+  if (isAssets) {
+    console.log('updating static assets');
+    cmd.runSync('npm run compile -- local static');
+  } else if (fileExt === 'html' || fileExt === 'svg' || fileExt === 'json') {
+    console.log('updating html');
+    cmd.runSync('npm run compile -- local html');
+    bs.reload('*.html');
+  } else if (fileExt === 'scss') {
+    console.log('updating css');
+    cmd.runSync('npm run compile -- local css');
+    bs.reload('*.css');
+  } else if (fileExt === 'js') {
+    console.log('updating js');
+    cmd.runSync('npm run compile -- local js');
+  } else {
+    console.log('non-watchable file extension changed :' + fileExt);
+  }
 });
