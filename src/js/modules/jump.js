784740f1bf7c1f16f480502c1068974e0da6aece
sammorrisdesign <mail@sammorrisdesign.co.uk>
2022-12-26 11:57:29 -0500

    switch from four to two spaces - js edition

--- src/js/modules/jump.js
+++ src/js/modules/jump.js
@@ -3,44 +3,44 @@ import offset from 'document-offset';
 let stepPositions = [];
 
 export default {
-    init: function() {
-        this.getPositionsForSteps();
-        this.bindings();
-    },
+  init: function () {
+    this.getPositionsForSteps();
+    this.bindings();
+  },
 
-    bindings: function() {
-        document.querySelectorAll('.is-step').forEach(function(step) {
-            step.addEventListener('click', function (e) {
-                this.jumpToNextProject(e.target);
-            }.bind(this));
-        }.bind(this));
+  bindings: function () {
+    document.querySelectorAll('.is-step').forEach(function (step) {
+      step.addEventListener('click', function (e) {
+        this.jumpToNextProject(e.target);
+      }.bind(this));
+    }.bind(this));
 
-        window.addEventListener('resize', function() {
-            this.getPositionsForSteps();
-        }.bind(this));
-    },
+    window.addEventListener('resize', function () {
+      this.getPositionsForSteps();
+    }.bind(this));
+  },
 
-    getPositionsForSteps: function() {
-        document.querySelectorAll('.is-step').forEach((step) => {
-            stepPositions.push(Math.floor(offset(step).top));
-        });
-    },
+  getPositionsForSteps: function () {
+    document.querySelectorAll('.is-step').forEach((step) => {
+      stepPositions.push(Math.floor(offset(step).top));
+    });
+  },
 
-    jumpToNextProject: function(el) {
-        if (el.classList.contains('is-step')) {
-            window.scrollTo({
-                top: this.getPointToJumpTo(offset(el).top),
-                behavior: 'smooth'
-            });
-        }
-    },
-
-    getPointToJumpTo: function(currentStepOffset) {
-        const scrollTop = window.pageYOffset;
-        let nextPoint = stepPositions.findIndex(function(position) {
-            return position === currentStepOffset
-        });
-
-        return stepPositions[nextPoint + 1] || 0;
+  jumpToNextProject: function (el) {
+    if (el.classList.contains('is-step')) {
+      window.scrollTo({
+        top: this.getPointToJumpTo(offset(el).top),
+        behavior: 'smooth'
+      });
     }
+  },
+
+  getPointToJumpTo: function (currentStepOffset) {
+    const scrollTop = window.pageYOffset;
+    let nextPoint = stepPositions.findIndex(function (position) {
+      return position === currentStepOffset
+    });
+
+    return stepPositions[nextPoint + 1] || 0;
+  }
 }
--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -3,99 +3,99 @@ import offset from 'document-offset';
 let height, scrollTop, projects;
 
 export default {
-    init: function() {
-        this.limitFiltersOnSafari();
-        this.updateFixedValues();
-        this.updateDynamicValues();
-        this.checkForActiveProject();
-        this.bindings();
-    },
+  init: function () {
+    this.limitFiltersOnSafari();
+    this.updateFixedValues();
+    this.updateDynamicValues();
+    this.checkForActiveProject();
+    this.bindings();
+  },
 
-    bindings: function() {
-        window.addEventListener('scroll', function() {
-            this.updateDynamicValues();
-            this.checkForActiveProject();
-            this.resetPlayedVideos();
-        }.bind(this),
-        {
-            passive: true
-        });
+  bindings: function () {
+    window.addEventListener('scroll', function () {
+      this.updateDynamicValues();
+      this.checkForActiveProject();
+      this.resetPlayedVideos();
+    }.bind(this),
+      {
+        passive: true
+      });
 
-        window.addEventListener('resize', function() {
-            this.updateFixedValues();
-            this.updateDynamicValues();
-            this.checkForActiveProject();
-        }.bind(this));
-    },
+    window.addEventListener('resize', function () {
+      this.updateFixedValues();
+      this.updateDynamicValues();
+      this.checkForActiveProject();
+    }.bind(this));
+  },
 
-    limitFiltersOnSafari: function() {
-        var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/) || navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/)
+  limitFiltersOnSafari: function () {
+    var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/) || navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/)
 
-        if (isSafari) {
-            document.body.classList.add("is-safari");
-        }
-    },
-
-    updateDynamicValues: function() {
-        scrollTop = window.pageYOffset;
-    },
-
-    updateFixedValues: function() {
-        height = window.innerHeight;
-
-        projects = [];
-
-        document.querySelectorAll('.project').forEach((project) => {
-            projects.push(Math.floor(offset(project).top));
-        });
-    },
-
-    checkForActiveProject: function() {
-        let activeProject;
-
-        projects.forEach((top, i) => {
-            if (scrollTop + (height / 2) > top) {
-                activeProject = i;
-            }
-        });
-
-        if (typeof activeProject === 'number') {
-            activeProject = document.querySelectorAll('.project')[activeProject];
-
-            if (!activeProject.classList.contains('project--active')) {
-                this.resetActiveProject();
-
-                activeProject.classList.add('project--active', 'project--played');
-                activeProject.querySelectorAll('.project__video').forEach((video) => {
-                    video.play();
-                });
-            }
-        } else {
-            this.resetActiveProject();
-        }
-    },
-
-    resetActiveProject: function() {
-        if (document.querySelector('.project--active')) {
-            document.querySelectorAll('.project--active .project__video').forEach((video) => {
-                video.pause();
-                video.currentTime = document.querySelector('.project--active .project__video').currentTime;
-            });
-            document.querySelector('.project--active').classList.remove('project--active');
-        }
-    },
-
-    resetPlayedVideos: function() {
-        document.querySelectorAll('.project--played').forEach((project) => {
-            const projectTop = offset(project.querySelector('.project__content')).top;
-            const projectBottom = projectTop + project.clientHeight;
-
-            if (projectTop > scrollTop + height || projectBottom < scrollTop) {
-                project.classList.remove('project--played');
-                project.querySelectorAll('.project__video').forEach((video) => {
-                    video.currentTime = 0;
-                })
-            }
-        });
+    if (isSafari) {
+      document.body.classList.add("is-safari");
     }
+  },
+
+  updateDynamicValues: function () {
+    scrollTop = window.pageYOffset;
+  },
+
+  updateFixedValues: function () {
+    height = window.innerHeight;
+
+    projects = [];
+
+    document.querySelectorAll('.project').forEach((project) => {
+      projects.push(Math.floor(offset(project).top));
+    });
+  },
+
+  checkForActiveProject: function () {
+    let activeProject;
+
+    projects.forEach((top, i) => {
+      if (scrollTop + (height / 2) > top) {
+        activeProject = i;
+      }
+    });
+
+    if (typeof activeProject === 'number') {
+      activeProject = document.querySelectorAll('.project')[activeProject];
+
+      if (!activeProject.classList.contains('project--active')) {
+        this.resetActiveProject();
+
+        activeProject.classList.add('project--active', 'project--played');
+        activeProject.querySelectorAll('.project__video').forEach((video) => {
+          video.play();
+        });
+      }
+    } else {
+      this.resetActiveProject();
+    }
+  },
+
+  resetActiveProject: function () {
+    if (document.querySelector('.project--active')) {
+      document.querySelectorAll('.project--active .project__video').forEach((video) => {
+        video.pause();
+        video.currentTime = document.querySelector('.project--active .project__video').currentTime;
+      });
+      document.querySelector('.project--active').classList.remove('project--active');
+    }
+  },
+
+  resetPlayedVideos: function () {
+    document.querySelectorAll('.project--played').forEach((project) => {
+      const projectTop = offset(project.querySelector('.project__content')).top;
+      const projectBottom = projectTop + project.clientHeight;
+
+      if (projectTop > scrollTop + height || projectBottom < scrollTop) {
+        project.classList.remove('project--played');
+        project.querySelectorAll('.project__video').forEach((video) => {
+          video.currentTime = 0;
+        })
+      }
+    });
+  }
 }
